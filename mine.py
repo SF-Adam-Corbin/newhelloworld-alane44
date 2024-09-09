7a10505dbb37a468f5e50930ad41f541b8a1c085
alane44 <alanemail44@gmail.com>
2024-09-09 01:59:32 -0400

    Update mine.py

--- mine.py
+++ mine.py
@@ -2,6 +2,7 @@
 
 
 def hello_world():
-  # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
-  
+  # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.=
+message = "Hello world!" 
+print(message)
   return variable_name
